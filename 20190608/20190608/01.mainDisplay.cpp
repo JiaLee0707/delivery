019506208c81d1ce86ef5e1aec6142b5e02169a2
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2019-06-23 03:07:08 +0900

    gotoxy 추가

--- 20190608/20190608/01.mainDisplay.cpp
+++ 20190608/20190608/01.mainDisplay.cpp
@@ -1,34 +1,68 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
-void MainDisplayView();
+#include <windows.h>
+void gotoxy(int x, int y) {
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
 
 int main() {
 	int money = 0;
-	MainDisplayView();
-	do
-	{
-		cout << "입금하실 돈을 입력해주세요 : ";
-		cin >> money;
-		if (money <= 0)
-			cout << "돈이 0원보다 작거나 같습니다." << endl << endl;
-	} while (money <= 0);
-	return 0;
-}
-void MainDisplayView() {
-	cout << "                                                                                                              " << endl;
-	cout << "                                                                                                              " << endl;
-	cout << "  ■    ■  ■    ■      ■■■■■■    ■        ■■■■      ■      ■■  ■■  ■  ■        ■■■    " << endl;
-	cout << "  ■    ■  ■    ■      ■              ■      ■        ■    ■      ■    ■    ■  ■      ■      ■  " << endl;
-	cout << "  ■    ■  ■    ■      ■              ■■■  ■        ■    ■      ■    ■    ■  ■      ■      ■  " << endl;
-	cout << "  ■    ■  ■    ■      ■              ■      ■        ■    ■      ■    ■    ■  ■              ■  " << endl;
-	cout << "  ■■■■  ■    ■      ■■■■■■■  ■      ■        ■■■■      ■    ■    ■  ■            ■    " << endl;
-	cout << "  ■    ■  ■■■■                      ■      ■        ■    ■      ■    ■    ■■■          ■      " << endl;
-	cout << "  ■    ■  ■    ■        ■■■■■■■■      ■        ■    ■      ■    ■    ■  ■          ■      " << endl;
-	cout << "  ■    ■  ■    ■                      ■      ■        ■    ■      ■    ■    ■  ■                  " << endl;
-	cout << "  ■    ■  ■    ■        ■■■■■■■■      ■        ■    ■      ■    ■    ■  ■          ■      " << endl;
-	cout << "  ■■■■  ■    ■        ■                      ■■■■      ■      ■■■■■■■  ■          ■      " << endl;
-	cout << "            ■    ■        ■■■■■■■■                      ■                  ■  ■                  " << endl;
-	cout << "                                                                                                              " << endl;
-	cout << "                                                                                                              " << endl;
+	unsigned char b[7];
+	for (int i = 0; i < 6; i++) {
+		b[i] = i + 1;
+	}
+	gotoxy(4, 3);
+	cout << (char)b[0];
+	for (int i = 0; i < 110; i++) {
+		cout << (char)b[5];
+	}
+	cout << (char)b[1];
+	gotoxy(4, 4);
+	for (int i = 0; i < 25; i++) {
+		cout << (char)b[4];
+		/*for (int j = 1; j < 111; j++) {
+			cout << " ";
+		}*/
+		gotoxy(5, 5);
+		cout << "  ■    ■  ■    ■      ■■■■■■    ■        ■■■■      ■      ■■  ■■  ■  ■        ■■■    " << endl;
+		gotoxy(5, 6);
+		cout << "  ■    ■  ■    ■      ■              ■■■  ■        ■    ■      ■    ■    ■  ■      ■      ■  " << endl;
+		gotoxy(5, 7);
+		cout << "  ■    ■  ■    ■      ■              ■      ■        ■    ■      ■    ■    ■  ■              ■  " << endl;
+		gotoxy(5, 8);
+		cout << "  ■■■■  ■    ■      ■■■■■■■  ■      ■        ■■■■      ■    ■    ■  ■            ■    " << endl;
+		gotoxy(5, 9);
+		cout << "  ■    ■  ■■■■                      ■      ■        ■    ■      ■    ■    ■■■          ■      " << endl;
+		gotoxy(5, 10);
+		cout << "  ■    ■  ■    ■        ■■■■■■■■      ■        ■    ■      ■    ■    ■  ■          ■      " << endl;
+		gotoxy(5, 11);
+		cout << "  ■    ■  ■    ■                      ■      ■        ■    ■      ■    ■    ■  ■                  " << endl;
+		gotoxy(5, 12);
+		cout << "  ■    ■  ■    ■        ■■■■■■■■      ■        ■    ■      ■    ■    ■  ■          ■      " << endl;
+		gotoxy(5, 13);
+		cout << "  ■■■■  ■    ■        ■                      ■■■■      ■      ■■■■■■■  ■          ■      " << endl;
+		gotoxy(5, 14);
+		cout << "            ■    ■        ■■■■■■■■                      ■                  ■  ■                  " << endl;
+		gotoxy(5, 15);
+		do
+		{
+			cout << "입금하실 돈을 입력해주세요 : ";
+			cin >> money;
+			if (money <= 0)
+				cout << "돈이 0원보다 작거나 같습니다." << endl << endl;
+		} while (money <= 0);
+		cout << (char)b[4];
+		gotoxy(4, 4 + i);
+	}
+	gotoxy(4, 26);
+	cout << (char)b[2];
+	for (int i = 0; i < 110; i++) {
+		cout << (char)b[5];
+	}
+	cout << (char)b[3];
 }
